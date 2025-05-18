0709df95c73202351698d1f1cf09a40a955c7a26
DmitryGytnikov <gytnikovd@mail.ru>
2025-05-18 15:51:29 +0300

    add code, using <slot>

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,97 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+import AppCard from './components/AppCard.vue'
 
-<template><div class="">Hello world</div></template>
+const cardsData = ref([
+  {
+    imgSrc: '../assets/img/area.jpg',
+  },
+  {
+    imgSrc: '../assets/img/panorama.jpg',
+  },
+  {
+    imgSrc: '',
+  },
+  {
+    imgSrc: '../assets/img/road.jpg',
+  },
+])
+</script>
 
-<style scoped></style>
+<template>
+  <div class="cards container">
+    <app-card v-for="(card, index) in cardsData" :img-src-new="card.imgSrc" :key="index">
+      <div v-if="!card.imgSrc" class="card">
+        <div class="text-wr">
+          <p>HTML ㅤㅤㅤCSS</p>
+          <p>SPA ㅤㅤㅤREST</p>
+          <p>AXIOS ㅤㅤㅤJSON</p>
+          <p>NPM ㅤㅤㅤBEM</p>
+        </div>
+      </div>
+    </app-card>
+  </div>
+
+  <div class="cards container">
+    <div class="card">
+      <img class="card-img" src="./assets/img/area.jpg" alt="picture" />
+    </div>
+    <div class="card">
+      <img class="card-img" src="./assets/img/panorama.jpg" alt="picture" />
+    </div>
+    <div class="card">
+      <div class="text-wr">
+        <p>HTML ㅤㅤㅤCSS</p>
+        <p>SPA ㅤㅤㅤREST</p>
+        <p>AXIOS ㅤㅤㅤJSON</p>
+        <p>NPM ㅤㅤㅤBEM</p>
+      </div>
+    </div>
+    <div class="card">
+      <img class="card-img" src="./assets/img/road.jpg" alt="picture" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.cards {
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  flex-wrap: wrap;
+  gap: 20px;
+
+  padding-top: 20px;
+}
+
+.container {
+  max-width: 1200px;
+  margin: 0 auto;
+  padding-left: 20px;
+  padding-right: 20px;
+}
+
+.card {
+  height: 300px;
+  width: 200px;
+  border: 1px solid black;
+  border-radius: 10px;
+  box-shadow: 3px 3px 20px rgba(50, 50, 50, 0.25);
+  overflow: hidden;
+}
+
+.card-img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  object-position: center;
+}
+
+.text-wr {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+}
+</style>
---
+++ src/components/AppCard.vue
@@ -0,0 +1,35 @@
+<script setup>
+const props = defineProps({
+  imgSrcNew: {
+    type: String,
+    required: false,
+  },
+})
+</script>
+
+<template>
+  <slot>
+    <div v-if="imgSrcNew" class="card">
+      <img class="card-img" :src="imgSrcNew" alt="picture" />
+      <div class="">{{ imgSrcNew }}</div>
+    </div>
+  </slot>
+</template>
+
+<style scoped>
+.card {
+  height: 300px;
+  width: 200px;
+  border: 1px solid black;
+  border-radius: 10px;
+  box-shadow: 3px 3px 20px rgba(50, 50, 50, 0.25);
+  overflow: hidden;
+}
+
+.card-img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  object-position: center;
+}
+</style>
